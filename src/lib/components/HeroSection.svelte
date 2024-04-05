5e0868f6dafe694f9b902b466bf4f1770a46f307
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-04-05 11:44:14 +0200

    fix:somes issues

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -12,7 +12,7 @@
     }, 2000);
 </script>
 
-<div class="bg-[url('/bg-mobile.png')] bg-cover bg-no-repeat bg-fixed w-screen h-screen md:hidden relative">
+<div style="background-image: url('/bg-mobile.png')" class=" bg-cover bg-no-repeat bg-fixed w-screen h-screen md:hidden relative">
     {#if StickyBannerVisible}
         <StickyBanner/>
     {/if}
