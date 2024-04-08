1165a0551b84b3e5abf37a6e5562a73cf20ab869
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-04-09 00:16:50 +0200

    feat(server): clients can communicates by using proper uuid's

--- src/lib/components/Infos.svelte
+++ src/lib/components/Infos.svelte
@@ -49,14 +49,14 @@
                         le bulletin de soutien IFI
                     </p>
                 </div>
-                <button on:click={slideNext} class="absolute right-3 top-0 bottom-0 my-auto">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34">
-                        <g id="Group_754" data-name="Group 754" transform="translate(-2692 360) rotate(-90)">
-                            <circle id="Ellipse_40" data-name="Ellipse 40" cx="17" cy="17" r="17" transform="translate(326 2692)" fill="#505050"/>
-                            <path id="Path_638" data-name="Path 638" d="M14162.583,5344l8.7,8.7,8.7-8.7" transform="translate(-13828.284 -2637)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="3"/>
-                        </g>
-                    </svg>
-                </button>
+<!--                <button on:click={slideNext} class="absolute right-3 top-0 bottom-0 my-auto">-->
+<!--                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34">-->
+<!--                        <g id="Group_754" data-name="Group 754" transform="translate(-2692 360) rotate(-90)">-->
+<!--                            <circle id="Ellipse_40" data-name="Ellipse 40" cx="17" cy="17" r="17" transform="translate(326 2692)" fill="#505050"/>-->
+<!--                            <path id="Path_638" data-name="Path 638" d="M14162.583,5344l8.7,8.7,8.7-8.7" transform="translate(-13828.284 -2637)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="3"/>-->
+<!--                        </g>-->
+<!--                    </svg>-->
+<!--                </button>-->
                 <div class="slider-item flex flex-col items-center justify-center ml-10">
                     <img src="/infos_svg/info3.svg" class="w-28 h-28">
                     <p class="font-Raleway text-center">
