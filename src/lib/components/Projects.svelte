eedcaebb73522f83e836c4f1afc662003f9ee750
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-04-09 17:36:47 +0200

    feat(message): working with the messages linked list

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -29,22 +29,22 @@
                     <span class="underline decoration-[#FF6600] ">2 équipements de pointe</span> pour révolutionner les traitements de<br/> demain contre les cancers grâce à des vaccins thérapeutiques
                 </p>
             </div>
-            <div class="flex flex-row items-center justify-center w-full xl:mt-5 px-5 xl:px-10 py-2 2xl:py-10 gap-5 relative h-[50rem] xl:h-[40rem] 2xl:h-[50rem]">
-                <div class="w-1/2 flex flex-col h-full gap-5 2xl:mt-4">
-                    <p class="text-xl xl:text-3xl font-Lato font-bold text-[#4D54D6] underline uppercase">
+            <div class="flex flex-row items-center justify-center w-full xl:mt-5 px-5 xl:px-10 py-2 2xl:py-10  relative h-[50rem] xl:h-[40rem] 2xl:h-[50rem]">
+                <div class="w-1/2 flex flex-col h-full 2xl:mt-4">
+                    <p class="text-xl xl:text-3xl font-Lato  font-bold text-[#4D54D6] underline uppercase">
                         L’Enjeu
                     </p>
-                    <p class="text-[#505050] font-Lato text-base 2xl:text-xl">
+                    <p class="text-[#505050] font-Lato text-base mt-5 2xl:text-xl">
                         Chaque tumeur possède un antigène qui lui est propre. Le vaccin à ARNm consiste à injecter dans le corps du patient un ARNm, fabriqué en laboratoire, qui contient le code nécessaire à la fabrication de cet antigène. Il force ainsi le système immunitaire du patient à reconnaître ces antigènes et à produire les anticorps correspondants.
                         <b>L’efficacité du vaccin à ARNm réside donc dans la capacité des chercheurs à identifier la séquence génétique permettant de coder des antigènes spécifiques de cellules tumorales à partir d’échantillons de tumeurs.</b>
                         <br/><br/>
                         Aujourd’hui il existe uniquement 3 équipements en Europe pour les fabriquer, ce qui occasionne beaucoup d’attente pour les patients qui sont en cours de traitement.
                     </p>
-                    <p class="text-xl xl:text-3xl font-Lato font-bold text-[#4D54D6] uppercase">
+                    <p class="text-xl xl:text-3xl font-Lato mt-5 font-bold text-[#4D54D6] uppercase">
                         Les nouvelles perspectives<br/>
                         <c class="underline">pour</c> les patients
                     </p>
-                    <ul>
+                    <ul class="mt-5">
                         <li class="text-[#505050] text-base 2xl:text-xl font-Lato font-bold">
                             Un délai de fabrication de vaccin raccourci de plusieurs mois.
                         </li>
@@ -57,27 +57,27 @@
                         </li>
                     </ul>
                 </div>
-                <div class="w-1/2 flex flex-col gap-20 h-full xl:gap-6 2xl:gap-10 relative">
+                <div class="w-1/2 flex flex-col h-full relative ml-5">
                     <p class="text-xl xl:text-3xl font-Lato font-bold text-[#4D54D6] underline uppercase">
                         L’innovation
                     </p>
-                    <p class="text-[#505050] font-Lato text-base 2xl:text-xl">
+                    <p class="text-[#505050] font-Lato text-base 2xl:text-xl mt-20 xl:mt-6 2xl:mt-10">
                         Les vaccins à ARN messager sont particulièrement prometteurs et leur développement ouvre de nouvelles perspectives de guérison pour les patients.
                         <b>L’Institut Curie souhaite donc acquérir 2 équipements de pointe indispensables à l’élaboration de ce type de vaccins : un purificateur de peptides et un spectromètre de masse.</b>
                     </p>
-                    <p class="text-xl xl:text-3xl  font-Lato font-bold text-[#4D54D6] underline uppercase 2xl:mt-0">
+                    <p class="text-xl xl:text-3xl  font-Lato font-bold text-[#4D54D6] underline uppercase mt-20 xl:mt-6 2xl:mt-10">
                         Le porteur du projet
                     </p>
-                    <div class="flex flex-row bg-[#4D54D6] items-center justify-center gap-3">
+                    <div class="flex flex-row bg-[#4D54D6] items-center justify-center mt-20 xl:mt-6 2xl:mt-10">
                         <img src="/pic-project-1.png" alt="Dr Antonin Morillon" class="w-[40%] h-full">
-                        <p class="text-white text-sm 2xl:text-xl font-Raleway p-2  2xl:p-4">
+                        <p class="text-white text-sm 2xl:text-xl font-Raleway p-2 my-3 2xl:p-4">
                             <span class="text-xl 2xl:text-2xl font-bold">
                                 Dr Antonin Morillon<br/>
                             </span>
                             Chef de l’équipe de recherche ARN non codant, épigénétique et fluidité des génomes à l’Institut Curie
                         </p>
                     </div>
-                    <div class="text-center bg-white font-Raleway font-bold py-3 xl:py-5 2xl:mt-0">
+                    <div class="text-center bg-white font-Raleway font-bold py-3 xl:py-5 mt-20 xl:mt-6 2xl:mt-10">
                         <p class="text-xl px-5 text-[#4D54D6]">
                             Coût de ces 2 équipements<br/>
                             <span class="underline text-2xl">1,4 millions €</span>
