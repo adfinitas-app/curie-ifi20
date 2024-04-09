2ecd878574cda286245ee5f350c75cad96759955
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-04-09 17:29:26 +0200

    feat(message): working with the messages linked list

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -82,7 +82,7 @@
         </svg>
 
         {#if StickyBannerVisible}
-            <div in:fade class="absolute bg-transparent lg:bottom-5 xl:bottom-5 2xl:bottom-10 flex flex-col items-center justify-center gap-3 2xl:gap-6 z-10">
+            <div in:fade class="absolute bg-transparent lg:bottom-5 xl:bottom-5 2xl:bottom-10 flex flex-col items-center justify-center z-10">
                 <p class="text-white lg:text-xl 2xl:text-[36px] font-Raleway font-semibold">Contre le cancer, investissez votre impôt dans l’innovation.</p>
                 <a href="#Intro"
                    on:click={()=> {
@@ -92,7 +92,7 @@
                         window.location.hash = "#projet1";
                     }, 100);}}
                 >
-                    <svg  xmlns="http://www.w3.org/2000/svg" width="94.94" height="21.854" viewBox="0 0 94.94 21.854">
+                    <svg class="mt-3 2xl:mt-6" xmlns="http://www.w3.org/2000/svg" width="94.94" height="21.854" viewBox="0 0 94.94 21.854">
                         <g id="Group_577" data-name="Group 577" transform="translate(-955.802 -686.267)">
                             <g id="Group_115" data-name="Group 115" transform="translate(956.534 688.129)">
                                 <g id="Group_116" data-name="Group 116" transform="translate(0 0)">
