d11f5cf8adcaeb0876d3d8685d210db2b99a07c3
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-04-09 18:07:58 +0200

    feat(message): working with the messages linked list

--- src/lib/components/GraphPart.svelte
+++ src/lib/components/GraphPart.svelte
@@ -87,7 +87,7 @@
                 Par exemple :
             </p>
             <img src="/graph3.png" class="w-[20rem] 2xl:w-[30rem]"/>
-            <a target="_blank" href="https://aider.curie.fr/is195" class="text-center bg-[#34B3C5] uppercase w-[268px] 2xl:w-[400px] text-[16px] md:text-xs 2xl:text-xl text-white lg:px-2 xl:py-2 xl:px-7 2xl:py-3 2xl:px-16 font-Raleway font-bold lg:hover:w-[300px] 2xl:hover:w-[450px] transition-all absolute -bottom-10 xl:-bottom-12  2xl:-bottom-20">
+            <a target="_blank" href="https://aider.curie.fr/is195" class="text-center bg-[#34B3C5] uppercase w-[268px] 2xl:w-[400px] text-[16px] md:text-xs 2xl:text-xl text-white lg:px-2 xl:py-2 xl:px-7 2xl:py-3 2xl:px-16 font-Raleway font-bold lg:hover:w-[300px] 2xl:hover:w-[450px] transition-all absolute -bottom-14 md:-bottom-10 xl:-bottom-12  2xl:-bottom-20">
                 JE FAIS UN DON<br/>
                 Déductible de<br/>
                 <span class="font-black">L’impôt sur les Sociétés</span>
