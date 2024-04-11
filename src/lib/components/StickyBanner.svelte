874821be272d483dbf3f9f9c4340f7ecf7c8f131
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-04-11 19:04:45 +0200

    push

--- src/lib/components/StickyBanner.svelte
+++ src/lib/components/StickyBanner.svelte
@@ -126,7 +126,7 @@
                             </p>
                             <div class="input-container rounded-3xl border-2 text-center 2xl:pt-2 border-gray-500 w-[25vh] h-[5vh] font-Raleway text-[#FF6600] text-xl 2xl:text-2xl font-bold">
                                 {#if montant2 !== undefined}
-                                    {Math.round(montantIfi - (montant2 * 0.75)).toLocaleString('fr-FR')}
+                                    {Math.round(montantIfi - ((montant2 > 66666 ? 66666 : montant2)* 0.75)).toLocaleString('fr-FR')}
                                 {/if}
                             </div>
                         </div>
