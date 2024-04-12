5e8d090366e698d059a53b342e512e37712f2370
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-04-12 11:54:27 +0200

    push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,12 +46,12 @@
 </script>
 
 <HeroSection/>
-<GraphPart/>
-<div class="hidden md:block relative">
-    <div bind:this={projectDesktopElement} id="ProjectsDesktop" class="absolute -top-40  w-screen"/>
-    <Projects/>
-</div>
-<div class="md:hidden flex items-center justify-center" bind:this={projectsMobileElement}>
-    <ProjectsMobile/>
-</div>
-<DoubleCtaPart/>
+<!--<GraphPart/>-->
+<!--<div class="hidden md:block relative">-->
+<!--    <div bind:this={projectDesktopElement} id="ProjectsDesktop" class="absolute -top-40  w-screen"/>-->
+<!--    <Projects/>-->
+<!--</div>-->
+<!--<div class="md:hidden flex items-center justify-center" bind:this={projectsMobileElement}>-->
+<!--    <ProjectsMobile/>-->
+<!--</div>-->
+<!--<DoubleCtaPart/>-->
